53926d08c6a98383cf1eb3c7dd2bd1978a4e0a68
msxb76@139.com <msxb76@139.com@c4c04f37-f9ec-48f9-bf45-3509e1cf2a81>
2016-07-19 16:24:42 +0000

    jxf

--- 1/index.php
+++ 1/index.php
@@ -1,3 +1,14 @@
 <?php
 header('content-type:text');
-echo 'Hello,Xiaocui';
+$timestamp = $_GET['timestamp'];
+$nonce = $_GET['nonce'];
+$token = 'jxf';
+$signature = $_GET['signature'];
+$array = array($timestamp,$nonce,$token);
+sort($array);
+$tmpstr = implode('', $array);
+$tmpstr sha1($tmpstr);
+if($tmpstr == $signature){
+	echo $_GET['echostr'];
+	exit;
+}
