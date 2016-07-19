c8cc18610c724419eeff6bae5990f2a53193ddcf
msxb76@139.com <msxb76@139.com@c4c04f37-f9ec-48f9-bf45-3509e1cf2a81>
2016-07-19 16:25:33 +0000

    cc

--- 1/index.php
+++ 1/index.php
@@ -7,7 +7,7 @@ $signature = $_GET['signature'];
 $array = array($timestamp,$nonce,$token);
 sort($array);
 $tmpstr = implode('', $array);
-$tmpstr sha1($tmpstr);
+$tmpstr = sha1($tmpstr);
 if($tmpstr == $signature){
 	echo $_GET['echostr'];
 	exit;
