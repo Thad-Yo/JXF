4180b7676141d0e334d0cde4315796af3763b38c
msxb76 <736602265@qq.com>
2016-07-21 16:04:12 +0000

    Type commit message here…

--- thinkphp/jxf.php
+++ thinkphp/jxf.php
@@ -1,3 +1,15 @@
 <?php
 header('content-type:text');
-echo phpinfo();
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
+
+define('BIND_MODULE','User');
+
+// 定义应用目录
+define('APP_NAME','JXF');
+define('APP_PATH','JXF/');
+
+// 引入ThinkPHP入口文件
+require './ThinkPHP/ThinkPHP.php';
