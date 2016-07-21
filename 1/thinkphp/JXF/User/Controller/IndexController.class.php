e15c73401e48593dd75c1501dc464cfde498ce98
msxb76@139.com <msxb76@139.com@c4c04f37-f9ec-48f9-bf45-3509e1cf2a81>
2016-07-21 16:51:42 +0000

    Type commit message here…

--- 1/thinkphp/JXF/User/Controller/IndexController.class.php
+++ 1/thinkphp/JXF/User/Controller/IndexController.class.php
@@ -3,6 +3,22 @@ namespace User\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        echo 'hello cuicui';
+        //1.获得参数
+        $nonce = $_GET['nonce'];
+        $token = 'jxf';
+        $timestamp = $_GET['timestamp'];
+        //形成数组，按子典排序
+        $array = array();
+        $array = array($nonce,$timestamp,$token);
+        sort($array);
+        //拼接成字符串，然后雨signature进行校验
+        $str = sha1(implode($array));
+        if($str == $signature){
+        	echo $echostr;
+        	exit;
+        }
+    }
+    public function show(){
+    	echo 'jxf';
     }
 }
