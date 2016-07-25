d1f50d07dff602ab57e38d522e539e1fb383e670
msxb76@139.com <msxb76@139.com@c4c04f37-f9ec-48f9-bf45-3509e1cf2a81>
2016-07-25 16:20:43 +0000

    Type commit message here…

--- 1/thinkphp/JXF/User/Controller/IndexController.class.php
+++ 1/thinkphp/JXF/User/Controller/IndexController.class.php
@@ -64,7 +64,7 @@ class IndexController extends Controller {
 		}
 		//用户发送tuwen1关键字的时候，回复一个单图文
 		if(strtolower($postObj->MsgType)=='text' && strtolower($postObj->Content) =='tuwen1'){
-			echo '嘻嘻';
+			echo '';
 			// $toUser = $postObj->FromUserName;
 			// $FromUser = $postObj->ToUserName;
 			// $time = time();
