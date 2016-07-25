1accac57670b34ed2e53f38f5439c0e5452e0601
msxb76@139.com <msxb76@139.com@c4c04f37-f9ec-48f9-bf45-3509e1cf2a81>
2016-07-25 16:37:35 +0000

    Type commit message here…

--- 1/thinkphp/JXF/User/Controller/IndexController.class.php
+++ 1/thinkphp/JXF/User/Controller/IndexController.class.php
@@ -79,7 +79,7 @@ class IndexController extends Controller {
 					array(
 					'title'=>'hupu',
 					'Description'=>'hupu is very yellow',
-					'PicUrl'=>'http://i1.hoopchina.com.cn/blogfile/201607/18/BbsImg146880770318550_1200x900.jpghttp://i1.hoopchina.com.cn/blogfile/201607/18/BbsImg146880770318550_1200x900.jpg',
+					'PicUrl'=>'http://i1.hoopchina.com.cn/blogfile/201607/18/BbsImg146880770318550_1200x900.jpg',
 					'Url'=>'http://www.hupu.com/',
 					),
 				);
