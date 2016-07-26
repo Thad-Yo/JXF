65e77e433c0fd08eae5b121b45ee33d79165b906
msxb76@139.com <msxb76@139.com@c4c04f37-f9ec-48f9-bf45-3509e1cf2a81>
2016-07-26 16:05:20 +0000

    Type commit message here…

--- 1/thinkphp/JXF/User/Controller/IndexController.class.php
+++ 1/thinkphp/JXF/User/Controller/IndexController.class.php
@@ -138,4 +138,18 @@ class IndexController extends Controller {
 				echo $info;
 		}		
 	}
+	function http_curl(){
+		//获取immc
+		//1、初始化curl
+		$ch = curl_init();
+		$url = 'http://nba.hupu.com'
+		//2、设置curl的参数
+		curl_setopt($ch, CURLOPT_URL,, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		//3、采集
+		$output = curl_exec($ch);	
+		//4、关闭
+		curl_close($ch);
+		var_dump($output);
+	}
 }
