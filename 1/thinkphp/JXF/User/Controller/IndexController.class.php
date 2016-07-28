3604e28facd53c03bf6b6b19dba42fc0e5961a17
msxb76@139.com <msxb76@139.com@c4c04f37-f9ec-48f9-bf45-3509e1cf2a81>
2016-07-28 16:19:03 +0000

    Type commit message here…

--- 1/thinkphp/JXF/User/Controller/IndexController.class.php
+++ 1/thinkphp/JXF/User/Controller/IndexController.class.php
@@ -63,40 +63,40 @@ class IndexController extends Controller {
 			}
 		}
 		//用户发送tuwen1关键字的时候，回复一个单图文
-		if(strtolower($postObj->MsgType)=='text'){			
+		if(strtolower($postObj->MsgType)=='text' && trim($postObj->Content) =='hupu'|| trim($postObj->Content) =='慕思'){			
 				$indexModel = new indexModel;
 				$indexModel->responseMsg($postObj);
 		}
-		// 	else{
-		// 	switch ( trim($postObj->Content) ) {
-		// 		case '电话':
-		// 			$Content = '18578665217';
-		// 			break;
-		// 		case 'QQ':
-		// 			$Content = '736602265';
-		// 			break;				
-		// 		case 'hupu':
-		// 			$Content = "<a href='http://nba.hupu.com/'>虎扑</a>";
-		// 			break;
-		// 		default:
-		// 			$Content = '这位朋友你在讲啥子？';
-		// 			break;
-		// 	}
-		// 		$template = '<xml>
-		// 					<ToUserName><![CDATA[%s]]></ToUserName>
-		// 					<FromUserName><![CDATA[%s]]></FromUserName>
-		// 					<CreateTime>%s</CreateTime>
-		// 					<MsgType><![CDATA[%s]]></MsgType>
-		// 					<Content><![CDATA[%s]]></Content>
-		// 					</xml>';
-		// 		$FromUser = $postObj->ToUserName;
-		// 		$toUser = $postObj->FromUserName;
-		// 		$time = time();
-		// 		// $Content = '18578665217';
-		// 		$MsgType = 'text';
-		// 		$info = sprintf($template,$toUser,$FromUser,$time,$MsgType,$Content);
-		// 		echo $info;
-		// }		
+			else{
+			switch ( trim($postObj->Content) ) {
+				case '电话':
+					$Content = '18578665217';
+					break;
+				case 'QQ':
+					$Content = '736602265';
+					break;				
+				case 'hupu':
+					$Content = "<a href='http://nba.hupu.com/'>虎扑</a>";
+					break;
+				default:
+					$Content = '这位朋友你在讲啥子？';
+					break;
+			}
+				$template = '<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							</xml>';
+				$FromUser = $postObj->ToUserName;
+				$toUser = $postObj->FromUserName;
+				$time = time();
+				// $Content = '18578665217';
+				$MsgType = 'text';
+				$info = sprintf($template,$toUser,$FromUser,$time,$MsgType,$Content);
+				echo $info;
+		}		
 	}
 	function http_curl(){
 		//获取immc
