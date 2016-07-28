c2afe55d514fcdc3bd5b5243a03d3a49045eb8cc
msxb76 <736602265@qq.com>
2016-07-28 14:51:51 +0000

    Type commit message here…

--- thinkphp/JXF/User/Controller/IndexController.class.php
+++ thinkphp/JXF/User/Controller/IndexController.class.php
@@ -64,8 +64,7 @@ class IndexController extends Controller {
 		}
 		//用户发送tuwen1关键字的时候，回复一个单图文
 		if(strtolower($postObj->MsgType)=='text' && trim($postObj->Content) =='hupu'|| trim($postObj->Content) =='慕思'){
-
-				
+		
 			//实例化模型
 			$indexModel = new indexModel;
 			$indexModel ->responseMsg($postObj);	
